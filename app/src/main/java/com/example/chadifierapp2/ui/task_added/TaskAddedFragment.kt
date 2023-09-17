c85d8b49133bb99a13fa72292d5ba1dc2cab9a39
nicolas <nicolas.castellano.6@hotmail.com>
2023-09-17 17:16:08 -0300

    fixing build exeption error on main fragment inflate

---
+++ app/src/main/java/com/example/chadifierapp2/ui/task_added/TaskAddedFragment.kt
@@ -0,0 +1,37 @@
+package com.example.chadifierapp2.ui.task_added
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.chadifierapp2.databinding.FragmentTaskAddedBinding
+
+//create a fragment for the task added page
+class TaskAddedFragment : Fragment() {
+//    this fragment will be used to display the task added page
+    private var _binding: FragmentTaskAddedBinding? = null
+
+    val binding get() = _binding!!
+
+//    the view model
+    private lateinit var taskAddedViewModel: TaskAddedViewModel
+
+//    override the fragment methods
+    override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+    ): View {
+//        inflate the layout for this fragment
+        _binding = FragmentTaskAddedBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
